e64a8a6f06c5853921861e2b3999236448e0d908
Eric <erichuan2020@gmail.com>
2021-12-03 15:51:07 -0500

    update delete test.py

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ import json
 
 
 class BasicTestCase(unittest.TestCase):
+
+    # Test GET users
     def test_get_users(self):
         tester = app.test_client(self)
         response = tester.get("/api/users", content_type='application/json')
@@ -15,11 +17,13 @@ class BasicTestCase(unittest.TestCase):
                          added_user['_id']['$oid'])
         self.assertEqual(response.status_code, 200)
 
+    # Test GET books
     def test_get_books(self):
         tester = app.test_client(self)
         response = tester.get("/api/books", content_type='application/json')
         self.assertEqual(response.status_code, 200)
 
+    # Test create a book
     def test_post_books(self):
         tester = app.test_client(self)
         book_payload = {
@@ -39,12 +43,15 @@ class BasicTestCase(unittest.TestCase):
 
         self.assertEqual(response.status_code, 200)
 
+    # Test GET a single book
+
     def test_get_book(self):
         tester = app.test_client(self)
         response = tester.get(
-            "/api/books/61a57799d052a6348ed7d0bd", content_type='application/json')
+            "/api/books/61a5746caebb9239739a8f96", content_type='application/json')
         self.assertEqual(response.status_code, 200)
 
+    # Test UPDATE a book
     def test_update_book(self):
         tester = app.test_client(self)
         book_payload = {
@@ -57,11 +64,38 @@ class BasicTestCase(unittest.TestCase):
             }
         }
 
-        response = tester.put('/api/books/61a5a716346017b73c69023b',
+        response = tester.put('/api/books/61a5746caebb9239739a8f96',
                               headers={"Content-Type": "application/json"
                                        },
                               data=json.dumps(book_payload))
 
+        self.assertEqual(response.status_code, 200)
+
+    # Test DELETE a book
+    def test_delete_book(self):
+        tester = app.test_client(self)
+
+        # first create a book
+        book_payload = {
+            "user_id": "61a56a765eb6cf68efab7ded",
+            "book": {
+                "title": "book3",
+                "author": "author3",
+                "isbn": "isbn3",
+                "publication_date": "book1_date3"
+            }
+        }
+
+        response = tester.post('/api/books',
+                               headers={"Content-Type": "application/json"
+                                        },
+                               data=json.dumps(book_payload))
+
+        self.assertEqual(response.status_code, 200)
+
+        # then delete the book
+        delete_id = response.json["id"]
+        response = tester.delete('/api/books/' + delete_id)
         self.assertEqual(response.status_code, 200)
 
 
